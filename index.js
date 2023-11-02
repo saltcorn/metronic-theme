c40c755cdf4d48f2bda60cee6b183a8414dc0da0
Tom Nielsen <tanielsen@gmail.com>
2023-11-02 09:44:12 +0000

    copy from material design

---
+++ index.js
@@ -0,0 +1,349 @@
+const {
+  div,
+  text,
+  a,
+  p,
+  footer,
+  section,
+  style,
+  h1,
+} = require("@saltcorn/markup/tags");
+const {
+  navbar,
+  navbarSolidOnScroll,
+} = require("@saltcorn/markup/layout_utils");
+const renderLayout = require("@saltcorn/markup/layout");
+const Field = require("@saltcorn/data/models/field");
+const Table = require("@saltcorn/data/models/table");
+const Form = require("@saltcorn/data/models/form");
+const View = require("@saltcorn/data/models/view");
+const Workflow = require("@saltcorn/data/models/workflow");
+const { renderForm, link } = require("@saltcorn/markup");
+const {
+  alert,
+  headersInHead,
+  headersInBody,
+} = require("@saltcorn/markup/layout_utils");
+const { features } = require("@saltcorn/data/db/state");
+
+const verstring = features?.version_plugin_serve_path
+  ? "@" + require("./package.json").version
+  : "";
+
+const blockDispatch = (config) => ({
+  pageHeader: ({ title, blurb }) =>
+    div(
+      h1({ class: "h3 mb-0 mt-2 text-gray-800" }, title),
+      blurb && p({ class: "mb-0 text-gray-800" }, blurb)
+    ),
+  footer: ({ contents }) =>
+    div(
+      { class: "container" },
+      footer(
+        { id: "footer" },
+        div({ class: "row" }, div({ class: "col-sm-12" }, contents))
+      )
+    ),
+  hero: ({ caption, blurb, cta, backgroundImage }) =>
+    section(
+      {
+        class:
+          "jumbotron text-center m-0 bg-info d-flex flex-column justify-content-center",
+      },
+      div(
+        { class: "container" },
+        h1({ class: "jumbotron-heading" }, caption),
+        p({ class: "lead" }, blurb),
+        cta
+      ),
+      backgroundImage &&
+        style(`.jumbotron {
+      background-image: url("${backgroundImage}");
+      background-size: cover;
+      min-height: 75vh !important;
+    }`)
+    ),
+  noBackgroundAtTop: () => true,
+  wrapTop: (segment, ix, s) =>
+    ["hero", "footer"].includes(segment.type)
+      ? s
+      : section(
+          {
+            class: [
+              "page-section",
+              `pt-2`,
+              ix === 0 && config.fixedTop && "mt-5",
+              segment.class,
+              segment.invertColor && "bg-primary",
+            ],
+            style: `${
+              segment.bgType === "Color"
+                ? `background-color: ${segment.bgColor};`
+                : ""
+            }
+            ${
+              segment.bgType === "Image" &&
+              segment.bgFileId &&
+              +segment.bgFileId
+                ? `background-image: url('/files/serve/${segment.bgFileId}');
+        background-size: ${segment.imageSize || "contain"};
+        background-repeat: no-repeat;`
+                : ""
+            }`,
+          },
+          div(
+            {
+              class: `${
+                segment.textStyle && segment.textStyle !== "h1"
+                  ? segment.textStyle
+                  : ""
+              }`,
+            },
+            segment.textStyle && segment.textStyle === "h1" ? h1(s) : s
+          )
+        ),
+});
+
+const renderBody = (title, body, alerts, config, role) =>
+  renderLayout({
+    blockDispatch: blockDispatch(config),
+    role,
+    layout:
+      typeof body === "string" && config.in_card
+        ? { type: "card", title, contents: body }
+        : body,
+    alerts,
+  });
+
+const wrapIt = (config, bodyAttr, headers, title, body) => `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Material Design Bootstrap -->
+    <link href="/plugins/public/material-design${verstring}/css/mdb.min.css" rel="stylesheet">
+
+    ${headersInHead(headers)}    
+    <title>${text(title)}</title>
+  </head>
+  <body ${bodyAttr}>
+    ${body}
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" 
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" 
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="/plugins/public/material-design${verstring}/js/mdb.min.js"></script>
+
+    ${headersInBody(headers)}
+    ${config.colorscheme === "navbar-light" ? navbarSolidOnScroll : ""}
+    <style>
+    table.table a {
+      color:#007bff;
+    }
+    body {
+      background-color: ${config.backgroundColor || "white"};
+    }
+    </style>
+  </body>
+</html>`;
+
+const authBrand = (config, { name, logo }) =>
+  logo
+    ? `<img class="mb-4" src="${logo}" alt="Logo" width="72" height="72">`
+    : "";
+
+const layout = (config) => ({
+  wrap: ({ title, menu, brand, alerts, currentUrl, body, headers, role }) =>
+    wrapIt(
+      config,
+      'id="page-top"',
+      headers,
+      title,
+      `
+    <div id="wrapper">
+        ${navbar(brand, menu, currentUrl, config)}
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12" id="page-inner-content">
+              ${renderBody(title, body, alerts, config, role)}
+            </div>
+          </div>
+        </div>
+    </div>
+    `
+    ),
+  renderBody: ({ title, body, alerts, role }) =>
+    renderBody(title, body, alerts, config, role),
+  authWrap: ({
+    title,
+    alerts, //TODO
+    form,
+    afterForm,
+    headers,
+    brand,
+    csrfToken,
+    authLinks,
+  }) =>
+    wrapIt(
+      config,
+      'class="text-center"',
+      headers,
+      title,
+      `
+  <div class="form-signin">
+    ${alerts.map((a) => alert(a.type, a.msg)).join("")}
+    ${authBrand(config, brand)}
+    <h3>
+      ${title}
+    </h3>
+    ${renderForm(formModify(form), csrfToken)}
+    ${renderAuthLinks(authLinks)}
+    ${afterForm}
+    <style>
+    html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-align: center;
+  -ms-flex-pack: center;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+    </style>
+  </div>
+  `
+    ),
+});
+const renderAuthLinks = (authLinks) => {
+  var links = [];
+  if (authLinks.login)
+    links.push(link(authLinks.login, "Already have an account? Login!"));
+  if (authLinks.forgot) links.push(link(authLinks.forgot, "Forgot password?"));
+  if (authLinks.signup)
+    links.push(link(authLinks.signup, "Create an account!"));
+  const meth_links = (authLinks.methods || [])
+    .map(({ url, icon, label }) =>
+      a(
+        { href: url, class: "btn btn-secondary btn-user btn-block mb-1" },
+        icon || "",
+        `&nbsp;Login with ${label}`
+      )
+    )
+    .join("");
+
+  return (
+    meth_links + links.map((l) => div({ class: "text-center" }, l)).join("")
+  );
+};
+const formModify = (form) => {
+  form.formStyle = "vert";
+  form.submitButtonClass = "btn-primary btn-user btn-block";
+  return form;
+};
+
+const configuration_workflow = () =>
+  new Workflow({
+    steps: [
+      {
+        name: "stylesheet",
+        form: async () => {
+          return new Form({
+            fields: [
+              {
+                name: "in_card",
+                label: "Default content in card?",
+                type: "Bool",
+                required: true,
+              },
+              {
+                name: "colorscheme",
+                label: "Navbar color scheme",
+                type: "String",
+                required: true,
+                default: "navbar-light",
+                attributes: {
+                  options: [
+                    { name: "navbar-dark bg-dark", label: "Dark" },
+                    { name: "navbar-dark bg-primary", label: "Dark Primary" },
+                    {
+                      name: "navbar-dark bg-secondary",
+                      label: "Dark Secondary",
+                    },
+                    { name: "navbar-light bg-light", label: "Light" },
+                    { name: "navbar-light bg-white", label: "White" },
+                    { name: "navbar-light", label: "Transparent Light" },
+                  ],
+                },
+              },
+              {
+                name: "fixedTop",
+                label: "Navbar Fixed Top",
+                type: "Bool",
+                required: true,
+              },
+              {
+                name: "backgroundColor",
+                label: "Background Color",
+                type: "Color",
+                default: "#ffffff",
+                required: true,
+              },
+            ],
+          });
+        },
+      },
+    ],
+  });
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  layout,
+  configuration_workflow,
+  plugin_name: "metronic-theme",
+};
