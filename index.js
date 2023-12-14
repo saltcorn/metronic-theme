2fdfaed90b0c0546b72502194b782015a11470da
Tom Nielsen <tanielsen@gmail.com>
2023-12-14 10:35:55 +0000

    mobile nav btn

--- index.js
+++ index.js
@@ -372,7 +372,20 @@ const secondaryMenuHeader = (
         class:
           "container-fluid d-flex align-items-stretch justify-content-between",
       },
-      div({ class: "d-flex align-items-center flex-grow-1 flex-lg-grow-0" }),
+      div(
+        {
+          class: "d-flex align-items-center d-lg-none ms-n1 me-2",
+          title: "Show aside menu",
+        },
+        div(
+          {
+            class:
+              "btn btn-icon btn-active-color-primary w-30px h-30px w-md-40px h-md-40px",
+            id: "kt_aside_mobile_toggle",
+          },
+          i({ class: "ki-outline ki-abstract-14 fs-1" })
+        )
+      ),
       div({ class: "d-flex align-items-center flex-grow-1 flex-lg-grow-0" }),
       div(
         {
