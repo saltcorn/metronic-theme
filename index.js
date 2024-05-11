138f1cc64577efc6f37a79ea3f0fff2eb646c316
Christian Hugo <hugo.christian@web.de>
2024-05-11 13:56:29 +0200

    no base href for iframes with srcdoc

--- index.js
+++ index.js
@@ -329,8 +329,6 @@ const wrapIt = (
 ) => `<!doctype html>
 <html lang="en" ${stylesheet?.htmlAttrs || ""}>
   <head>
-    ${!isNode ? `<base href="http://localhost">` : ""}
-
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
